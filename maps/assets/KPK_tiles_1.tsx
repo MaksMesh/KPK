9e1d6efa2caa3950693d15dc8825cf856a0ff64b
RKP16102052 <prokhorov-andrey-lms@yandex.ru>
2026-01-25 18:46:38 +0300

    Add files via upload
    WIP: Planet map files have been added

---
+++ maps/assets/KPK_tiles_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="KPK_tiles_1" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="./assets/tile1.jpg" width="128" height="128"/>
+</tileset>
